4a94bf82e0492e6d3fa1e1fa2563f5c6f4946416
Martin <125393209+DWarmech@users.noreply.github.com>
2024-01-28 00:59:34 +0100

    Create integer_operations.py

---
+++ integer_operations.py
@@ -0,0 +1,10 @@
+first = int(input())
+second = int(input())
+third = int(input())
+fourth = int(input())
+
+plus = first + second
+divide = int(plus / third)
+multiply = divide * fourth
+
+print(multiply)
