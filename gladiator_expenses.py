bfd147f034b900fae39d3d5f96d055c8c69ff841
Martin <125393209+DWarmech@users.noreply.github.com>
2024-01-28 00:59:15 +0100

    Create gladiator_expenses.py

---
+++ gladiator_expenses.py
@@ -0,0 +1,21 @@
+lost_fights = int(input())
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+expenses = 0.00
+shield_counter = 0
+
+for day in range(1, lost_fights + 1):
+    if day % 2 == 0:
+        expenses += helmet_price
+    if day % 3 == 0:
+        expenses += sword_price
+    if day % 6 == 0:
+        expenses += shield_price
+        shield_counter += 1
+        if shield_counter == 2:
+            expenses += armor_price
+            shield_counter = 0
+
+print(f"Gladiator expenses: {expenses:.2f} aureus")
