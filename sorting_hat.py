680a32fe966439610f4b9ae2b3bf5b0d9a26ab00
Martin <125393209+DWarmech@users.noreply.github.com>
2024-01-28 01:01:39 +0100

    Create sorting_hat.py

---
+++ sorting_hat.py
@@ -0,0 +1,21 @@
+word = input()
+
+while word != "Welcome!":
+    if word == "Voldemort":
+        print(f"You must not speak of that name!")
+        break
+
+    if len(word) < 5:
+        print(f"{word} goes to Gryffindor.")
+    elif len(word) == 5:
+        print(f"{word} goes to Slytherin.")
+    elif len(word) == 6:
+        print(f"{word} Ravenclaw.")
+    elif len(word) > 6:
+        print(f"{word} goes to Hufflepuff.")
+
+    word = input()
+
+if word != "Voldemort":
+    print(f"Welcome to Hogwarts.")
+
